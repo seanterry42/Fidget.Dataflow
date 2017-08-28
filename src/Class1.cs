4452a38dca738d6cc5ec9238fff04f2c24bfe7e3
Sean Terry <sean.terry@seaseducation.com>
2017-08-28 15:34:44 -0500

    Solution structure

---
+++ src/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Fidget.Dataflow
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Fidget.Dataflow.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
